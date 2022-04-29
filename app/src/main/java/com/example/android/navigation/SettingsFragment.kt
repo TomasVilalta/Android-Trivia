9ba23fcb0c251f8e74c33c86571a6ea14605542a
TomasVilalta <tomasfedericovilalta@gmail.com>
2022-04-29 11:04:35 -0300

    Animated views

--- app/src/main/java/com/example/android/navigation/SettingsFragment.kt
+++ app/src/main/java/com/example/android/navigation/SettingsFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.android.navigation.databinding.FragmentGameWonBinding
+import com.example.android.navigation.databinding.SecretSettingsBinding
 
 class SettingsFragment : Fragment() {
 
@@ -13,6 +18,13 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.secret_settings, container, false)
+            val binding: SecretSettingsBinding = DataBindingUtil.inflate(
+                inflater, R.layout.secret_settings, container, false
+            )
+        binding.setImage.startAnimation(AnimationUtils.loadAnimation(context,R.anim.rotate))
+        binding.switch1.setOnClickListener{ Toast.makeText(context,"hi",Toast.LENGTH_SHORT).show()}
+
+        return binding.root
+
     }}
 
